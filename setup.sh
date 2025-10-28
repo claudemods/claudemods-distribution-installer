271ad119d7c2531121066e37ca9dad565a989964
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-28 19:23:10 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-sudo pacman -Sy
-sudo pacman -S git unzip
 cd /home/$USER && git clone https://github.com/claudemods/claudemods-distribution-installer
 sudo mkdir /opt/claudemods-distribution-installer
 sudo cp -r /home/$USER/claudemods-distribution-installer/installer/* /opt/claudemods-distribution-installer
 sudo chmod +x /opt/claudemods-distribution-installer/*
 sudo rm -rf /home/$USER/claudemods-distribution-installer
-./opt/claudemods-distribution-installer/mainscript.sh
+cd /opt/claudemods-distribution-installer && ./mainscript.sh

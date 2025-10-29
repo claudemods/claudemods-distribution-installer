1b073b1ef8b35d0571a2ac79e0a22ee847aadd40
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-29 17:55:47 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 cd /home/$USER && git clone https://github.com/claudemods/claudemods-distribution-installer
 sudo mkdir /opt/claudemods-distribution-installer
 sudo cp -r /home/$USER/claudemods-distribution-installer/installer/* /opt/claudemods-distribution-installer
-cd /opt/claudemods-distribution-installer && g++ -o mainscript mainscript.cpp -std=c++23
+cd /opt/claudemods-distribution-installer && sudo g++ -o mainscript mainscript.cpp -std=c++23
 sudo chmod +x /opt/claudemods-distribution-installer/*
 sudo rm -rf /home/$USER/claudemods-distribution-installer
-cd /opt/claudemods-distribution-installer && sudo g++ -o mainscript mainscript.cpp -std=c++23 && sudo ./mainscript
+cd /opt/claudemods-distribution-installer && sudo ./mainscript

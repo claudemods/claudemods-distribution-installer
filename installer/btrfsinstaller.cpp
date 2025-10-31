c87eac8d2cca28c97870370466c2c7567f95604b
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-31 00:06:59 +0000

    more fixes to cachyos kde

--- installer/btrfsinstaller.cpp
+++ installer/btrfsinstaller.cpp
@@ -878,9 +878,9 @@ private:
         execute_command("mkdir /mnt/home/" + new_username + "/.config/autostart");
         execute_command("cp -r /opt/claudemods-distribution-installer /mnt/opt");
         execute_command("cp -r /opt/claudemods-distribution-installer/install-fullkde-grub/cachyoskdegrub.desktop /mnt/home/" + new_username + "/.config/autostart");
-        execute_command("chown " + new_username + ":" + new_username + " /mnt/home/" + new_username + "/.config");
-        execute_command("chown " + new_username + ":" + new_username + " /mnt/home/" + new_username + "/.config/autostart");
-        execute_command("chown " + new_username + ":" + new_username + " /mnt/home/" + new_username + "/.config/autostart/cachyoskdegrub.desktop");
+        execute_command("chown " + new_username + "/mnt/home/" + new_username + "/.config");
+        execute_command("chown " + new_username + "/mnt/home/" + new_username + "/.config/autostart");
+        execute_command("chown " + new_username + "/mnt/home/" + new_username + "/.config/autostart/cachyoskdegrub.desktop");
         execute_command("chmod +x /mnt/home/" + new_username + "/.config/autostart/cachyoskdegrub.desktop");
         execute_command("chmod +x /opt/claudemods-distribution-installer/install-fullkde-grub/*");
         

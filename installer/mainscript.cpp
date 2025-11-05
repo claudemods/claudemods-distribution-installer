6c9c7e41d05d56f474da4b1a22d330759964253c
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-05 20:55:46 +0000

    going to be testing

--- installer/mainscript.cpp
+++ installer/mainscript.cpp
@@ -884,6 +884,7 @@ private:
         execute_command("cd /mnt && wget --show-progress --no-check-certificate 'https://drive.usercontent.google.com/download?id=1hu-2iRiJ0bFGK0Na5NzIlcHNgQQ5V90J&export=download&authuser=0&confirm=t&uuid=13272b82-4d80-4b24-94dd-723d66506aef&at=AKSUxGM1CkztZN2R0FiFt3pZ3Z6X:1762356023814'");
         execute_command("cd mnt && mv download* /mnt/rootfs.img >/dev/null 2>&1");
         execute_command("cd /mnt && unsquashfs -f -d /mnt /mnt/rootfs.img");
+        execute_command("rm -rf /mnt/rootfs.img");
         
 
         execute_command("mount " + efi_part + " /mnt/boot/efi");

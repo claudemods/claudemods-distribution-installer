be65dba831ab1cd7d9e74fd70c8bf02257937a28
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-05 22:55:53 +0000

    Delete installer/install-fullkde-grub directory

--- installer/install-fullkde-grub/cachyoskdegrub.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-konsole -e /opt/claudemods-distribution-installer/install-fullkde-grub/install-repo-and-full-cachyos-kde-grub-packages.sh
--- installer/install-fullkde-grub/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-nohup /usr/bin/plasmashell > /dev/null 2>&1 &
-sleep 1
-sudo -S rm -rf /opt/claudemods-distribution-installer

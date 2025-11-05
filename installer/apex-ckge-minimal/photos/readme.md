dc216b3048cb23bf7c5e352a2bb42604cb6f93c7
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-05 22:55:23 +0000

    not needed anymore

--- installer/apex-ckge-minimal/photos/readme.md
+++
@@ -1,2 +0,0 @@
-
-<img width="1280" height="800" alt="apex" src="https://github.com/user-attachments/assets/67dac877-f39e-4e6a-943d-0c94160cfb71" />
--- installer/apex-ckge-minimal/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp -r /opt/claudemods-distribution-installer/apex-ckge-minimal/main.xml /home/$USER/.local/share/plasma/plasmoids/deskhide/contents/config
-nohup /usr/bin/plasmashell > /dev/null 2>&1 &
-sleep 1
-plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/Apex/rift.jpg > /dev/null 2>&1
-plasma-apply-colorscheme Apex > /dev/null 2>&1
-sudo -S rm -rf /opt/claudemods-distribution-installer
-sudo -S rm -rf /home/$USER/appimages.zip
-sudo -S rm -rf /home/$USER/apps

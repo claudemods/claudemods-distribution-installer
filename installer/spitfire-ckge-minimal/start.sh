f2084b08ba4fdf74158d4a42a1cb4d978bd13307
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-05 23:34:03 +0000

    fixes

--- installer/spitfire-ckge-minimal/start.sh
+++ installer/spitfire-ckge-minimal/start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-cp -r /home/$USER/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge/kdegrub-minimal/main.xml /home/$USER/.local/share/plasma/plasmoids/deskhide/contents/config
+cp -r /home/$USER/claudemods-distribution-installer/installer/spitfire-ckge-minimal/main.xml /home/$USER/.local/share/plasma/plasmoids/deskhide/contents/config
 nohup /usr/bin/plasmashell > /dev/null 2>&1 &
 sleep 1
 plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/SpitFire/spitfire.png > /dev/null 2>&1
 plasma-apply-colorscheme SpitFire > /dev/null 2>&1
-cp -r /home/$USER/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge/kdegrub-minimal/konsolerc /home/$USER/.config
-sudo rm -rf /home/$USER/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge
-sudo rm -rf /home/$USER/appimages.zip
-sudo rm -rf /home/$USER/apps
+cp -r /home/$USER/claudemods-distribution-installer/installer/spitfire-ckge-minimal/konsolerc /home/$USER/.config
+sudo rm -rf /home/$USER/claudemods-distribution-installer

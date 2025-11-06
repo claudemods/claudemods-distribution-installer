d534d232057d1d947f6b8b5bbbca28730ee19e11
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-06 19:14:19 +0000

    more fixes testing now

--- installer/spitfire-ckge-minimal/start.sh
+++ installer/spitfire-ckge-minimal/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-cp -r /home/$USER/claudemods-distribution-installer/installer/spitfire-ckge-minimal/main.xml /home/$USER/.local/share/plasma/plasmoids/deskhide/contents/config
+USERNAME=$1
+
+cp -r /home/$USERNAME/claudemods-distribution-installer/installer/spitfire-ckge-minimal/main.xml /home/$USERNAME/.local/share/plasma/plasmoids/deskhide/contents/config
 nohup /usr/bin/plasmashell > /dev/null 2>&1 &
 sleep 1
 plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/SpitFire/spitfire.png > /dev/null 2>&1
-cp -r /home/$USER/claudemods-distribution-installer/installer/spitfire-ckge-minimal/konsolerc /home/$USER/.config
-cd /home/$USER/claudemods-distribution-installer/installer && chmod +x cleanup.sh
-cd /home/$USER/claudemods-distribution-installer/installer && ./cleanup.sh
+cp -r /home/$USERNAME/claudemods-distribution-installer/installer/spitfire-ckge-minimal/konsolerc /home/$USERNAME/.config
+cd /home/$USERNAME/claudemods-distribution-installer/installer && chmod +x cleanup.sh
+cd /home/$USERNAME/claudemods-distribution-installer/installer && ./cleanup.sh

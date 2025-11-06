4ad107dc20f3a291f2844bc6bccda8a54328b3e1
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-06 00:25:26 +0000

    testing

--- installer/spitfire-ckge-minimal/start.sh
+++ installer/spitfire-ckge-minimal/start.sh
@@ -3,6 +3,5 @@ cp -r /home/$USER/claudemods-distribution-installer/installer/spitfire-ckge-mini
 nohup /usr/bin/plasmashell > /dev/null 2>&1 &
 sleep 1
 plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/SpitFire/spitfire.png > /dev/null 2>&1
-plasma-apply-colorscheme SpitFire > /dev/null 2>&1
 cp -r /home/$USER/claudemods-distribution-installer/installer/spitfire-ckge-minimal/konsolerc /home/$USER/.config
 sudo -S rm -rf /home/$USER/claudemods-distribution-installer

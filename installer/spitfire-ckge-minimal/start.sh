34e79c75826801da763cd026504527ce66fe5e73
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-06 16:29:55 +0000

    should be the fix

--- installer/spitfire-ckge-minimal/start.sh
+++ installer/spitfire-ckge-minimal/start.sh
@@ -4,3 +4,5 @@ nohup /usr/bin/plasmashell > /dev/null 2>&1 &
 sleep 1
 plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/SpitFire/spitfire.png > /dev/null 2>&1
 cp -r /home/$USER/claudemods-distribution-installer/installer/spitfire-ckge-minimal/konsolerc /home/$USER/.config
+cd /home/$USER/claudemods-distribution-installer/installer && chmod +x cleanup.sh
+cd /home/$USER/claudemods-distribution-installer/installer && ./cleanup.sh

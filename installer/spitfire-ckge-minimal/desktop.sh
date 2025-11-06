b921e5b0ac167cb7836287438ec1899fd9dd858a
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-06 21:16:17 +0000

    all installs fine now but this is last needed thing

--- installer/spitfire-ckge-minimal/desktop.sh
+++ installer/spitfire-ckge-minimal/desktop.sh
@@ -2,4 +2,5 @@
 echo final step needed for wallpaper and clean up
 plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/SpitFire/spitfire.png > /dev/null 2>&1
 cd /home/$USER/claudemods-distribution-installer/installer && chmod +x cleanup.sh
+rm -rf /home/$USER/.config/autostart/wallpaper.desktop
 cd /home/$USER/claudemods-distribution-installer/installer && ./cleanup.sh

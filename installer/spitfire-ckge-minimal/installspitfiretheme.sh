0c33ab098b8d9b1dfe4613925c62349ddd4dae9a
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-06 19:01:41 +0000

    more changes

--- installer/spitfire-ckge-minimal/installspitfiretheme.sh
+++ installer/spitfire-ckge-minimal/installspitfiretheme.sh
@@ -195,7 +195,7 @@ install_backup() {
     done
 
     LOG_TEXT+="Applying KDE configurations...\n"
-    cd /home/$USER/claudemods-distribution-installer/installer/spitfire-ckge-minimal && sudo -S ./start.sh
+    cd /home/$USER/claudemods-distribution-installer/installer/spitfire-ckge-minimal && sudo -S ./start.sh "$1"
     LOG_TEXT+="✓ KDE configuration applied successfully\n"
 }
 
@@ -207,4 +207,4 @@ install_theme() {
 }
 
 # Main execution
-install_theme
+install_theme "$1"

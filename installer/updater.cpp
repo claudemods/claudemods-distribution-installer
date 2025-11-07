42f5e0ddab4306132c043dff88a9a4a3648eed63
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-07 09:30:58 +0000

    need to test

--- installer/updater.cpp
+++ installer/updater.cpp
@@ -5,6 +5,7 @@
 #include <pthread.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <pwd.h>
 #include <array>
 #include <memory>
 #include <string>
@@ -25,8 +26,23 @@ char downloaded_version[64] = "unknown";
 char installed_version[64] = "unknown";
 char username[256] = "";
 
+// Function to get current username
 const char* get_username() {
-    return getenv("USER");
+    if (username[0] == '\0') {
+        const char* user_env = getenv("USER");
+        if (user_env) {
+            strncpy(username, user_env, sizeof(username) - 1);
+        } else {
+            // Fallback to getpwuid if USER environment variable is not set
+            struct passwd *pw = getpwuid(getuid());
+            if (pw) {
+                strncpy(username, pw->pw_name, sizeof(username) - 1);
+            } else {
+                strcpy(username, "unknown");
+            }
+        }
+    }
+    return username;
 }
 
 void silent_command(const char* cmd) {
@@ -38,15 +54,13 @@ void silent_command(const char* cmd) {
 std::string run_command(const char* cmd) {
     std::array<char, 128> buffer;
     std::string result;
-    FILE* pipe = popen(cmd, "r");
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
     if (!pipe) {
         throw std::runtime_error("popen() failed!");
     }
-    while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
         result += buffer.data();
     }
-    pclose(pipe);
-
     if (!result.empty() && result.back() == '\n') {
         result.pop_back();
     }
@@ -55,23 +69,23 @@ std::string run_command(const char* cmd) {
 
 void* execute_update_thread(void* /*arg*/) {
     while (!loading_complete) usleep(10000);
-
+    
     const char* user = get_username();
-
+    
     // 1. GIT CLONE
     char git_cmd[512];
     snprintf(git_cmd, sizeof(git_cmd), "cd /home/%s/ && git clone https://github.com/claudemods-distribution-installer.git", user);
     silent_command(git_cmd);
-
+    
     // 2. CURRENT VERSION
     try {
         std::string version_output = run_command("cat /opt/claudemods-distribution-installer/version.txt");
-        strncpy(current_version, version_output.empty() ? "not installed" : version_output.c_str(),
+        strncpy(current_version, version_output.empty() ? "not installed" : version_output.c_str(), 
                 sizeof(current_version) - 1);
     } catch (...) {
         strcpy(current_version, "not installed");
     }
-
+    
     // 3. DETECT DISTRO (ONLY ARCH AND CACHYOS SUPPORTED)
     try {
         std::string distro_output = run_command("cat /etc/os-release | grep '^ID=' | cut -d'=' -f2 | tr -d '\"'");
@@ -86,7 +100,7 @@ void* execute_update_thread(void* /*arg*/) {
         std::cout << COLOR_RED << "\nError: Could not detect distribution.\n" << COLOR_RESET;
         exit(EXIT_FAILURE);
     }
-
+    
     // 4. DOWNLOADED VERSION
     if (strcmp(detected_distro, "arch") == 0 || strcmp(detected_distro, "cachyos") == 0) {
         try {
@@ -98,39 +112,39 @@ void* execute_update_thread(void* /*arg*/) {
             strcpy(downloaded_version, "unknown");
         }
     }
-
+    
     // INSTALLATION PROCESS
     run_command("sudo rm -rf /opt/claudemods-distribution-installer/mainscript");
     run_command("sudo rm -rf /opt/claudemods-distribution-installer/updater");
-
+    
     // ARCH AND CACHYOS INSTALLATION
     if (strcmp(detected_distro, "arch") == 0 || strcmp(detected_distro, "cachyos") == 0) {
         char copy_version_cmd[512];
         snprintf(copy_version_cmd, sizeof(copy_version_cmd), "cp -r /home/%s/claudemods-distribution-installer/installer/version.txt /opt/claudemods-distribution-installer", user);
         silent_command(copy_version_cmd);
-
+        
         char compile_main_cmd[512];
         snprintf(compile_main_cmd, sizeof(compile_main_cmd), "cd /home/%s/claudemods-distribution-installer/installer && sudo g++ -o mainscript mainscript.cpp -std=c++23", user);
         silent_command(compile_main_cmd);
-
+        
         char compile_updater_cmd[512];
         snprintf(compile_updater_cmd, sizeof(compile_updater_cmd), "cd /home/%s/claudemods-distribution-installer/installer && sudo g++ -o updater updater.cpp -std=c++23", user);
         silent_command(compile_updater_cmd);
-
+        
         char copy_main_cmd[512];
         snprintf(copy_main_cmd, sizeof(copy_main_cmd), "sudo cp /home/%s/claudemods-distribution-installer/installer/mainscript /opt/claudemods-distribution-installer", user);
         silent_command(copy_main_cmd);
-
+        
         char copy_updater_cmd[512];
         snprintf(copy_updater_cmd, sizeof(copy_updater_cmd), "sudo cp /home/%s/claudemods-distribution-installer/installer/updater /opt/claudemods-distribution-installer", user);
         silent_command(copy_updater_cmd);
     }
-
+    
     // Cleanup
     char cleanup_cmd[512];
     snprintf(cleanup_cmd, sizeof(cleanup_cmd), "rm -rf /home/%s/claudemods-multi-iso-konsole-script", user);
     silent_command(cleanup_cmd);
-
+    
     // GET INSTALLED VERSION
     try {
         std::string installed_version_output = run_command("cat /opt/claudemods-distribution-installer/version.txt");
@@ -138,7 +152,7 @@ void* execute_update_thread(void* /*arg*/) {
     } catch (...) {
         strcpy(installed_version, "unknown");
     }
-
+    
     commands_completed = true;
     return nullptr;
 }
@@ -157,12 +171,12 @@ void show_loading_bar() {
 int main() {
     pthread_t thread;
     pthread_create(&thread, nullptr, execute_update_thread, nullptr);
-
+    
     show_loading_bar();
-
+    
     while (!commands_completed) usleep(10000);
     pthread_join(thread, nullptr);
-
+    
     // >>> ORIGINAL SUMMARY <<<
     std::cout << COLOR_GREEN << "\nInstallation complete!\n" << COLOR_RESET;
     std::cout << COLOR_GREEN << "Executable installed in location: /opt/claudemods-distribution-installer/mainscript\n" << COLOR_RESET;
@@ -170,14 +184,14 @@ int main() {
     std::cout << COLOR_GREEN << "Current version: " << current_version << COLOR_RESET << std::endl;
     std::cout << COLOR_GREEN << "Downloaded version: " << downloaded_version << COLOR_RESET << std::endl;
     std::cout << COLOR_GREEN << "Installed version: " << installed_version << COLOR_RESET << std::endl;
-
+    
     std::cout << COLOR_CYAN << "\nLaunch now? (y/n): " << COLOR_RESET;
     char response;
     std::cin >> response;
-
+    
     if (response == 'y' || response == 'Y') {
         system("cd /opt/claudemods-distribution-installer/mainscript && ./mainscript");
     }
-
+    
     return EXIT_SUCCESS;
 }

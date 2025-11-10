31cbea8f5b5a9dfb3d039ea6d12848c739b49660
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-10 19:45:33 +0000

    Create install-mainbranch.sh

---
+++ install-mainbranch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/$USER && git clone https://github.com/claudemods/claudemods-distribution-installer
+sudo mkdir /opt/claudemods-distribution-installer
+sudo cp -r /home/$USER/claudemods-distribution-installer/installer/* /opt/claudemods-distribution-installer
+cd /opt/claudemods-distribution-installer && sudo g++ -o mainscript mainscript.cpp -std=c++23
+cd /opt/claudemods-distribution-installer && sudo g++ -o updater updater.cpp -std=c++23
+sudo chmod +x /opt/claudemods-distribution-installer/*
+sudo rm -rf /home/$USER/claudemods-distribution-installer
+cd /opt/claudemods-distribution-installer && sudo ./mainscript

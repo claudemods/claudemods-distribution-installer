6c264ae2a326e99b589e216128ecc48f6922cc32
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-11 16:01:18 +0000

    claudemods options fixes

--- installer/btrfsinstaller.h
+++ installer/btrfsinstaller.h
@@ -947,7 +947,7 @@ private:
         // Use execute_cd_command for cd commands
         execute_cd_command("cd /mnt");
         execute_command("wget --show-progress --no-check-certificate --continue --tries=3 --timeout=30 --waitretry=5 https://claudemodsreloaded.co.uk/claudemods-rootfs-images/claudemods-apex-ckge-minimal/apex.img");
-        execute_command("unsquashfs -f -d /mnt /mnt/claudemods-v1.img");
+        execute_command("unsquashfs -f -d /mnt /mnt/apex.img");
         execute_command("mount " + efi_part + " /mnt/boot/efi");
 
         install_grub_btrfs(drive);
@@ -979,7 +979,7 @@ private:
 
         execute_cd_command("cd /mnt");
         execute_command("wget --show-progress --no-check-certificate --continue --tries=3 --timeout=30 --waitretry=5 https://claudemodsreloaded.co.uk/claudemods-rootfs-images/claudemods-apex-ckge-minimal/apex.img");
-        execute_command("unsquashfs -f -d /mnt /mnt/claudemods-v1.img");
+        execute_command("unsquashfs -f -d /mnt /mnt/apex.img");
         execute_command("mount " + efi_part + " /mnt/boot/efi");
 
         install_grub_btrfs(drive);

f5d67a97ea94732a70425e689138911e5f89d973
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-11 01:23:01 +0000

    Update installdevpackages.sh

--- installer/installdevpackages.sh
+++ installer/installdevpackages.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-sudo -S pacman -Rns --noconfirm plasma-desktop dolphin kate konsole attica knewstuff discover
+sudo -S pacman -Rsc --noconfirm phonon-qt6
+sudo -S pacman -Rsc --noconfirm breeze-gtk
+sudo -S pacman -Rsc --noconfirm kcoreaddons
+sudo -S pacman -Rsc --noconfirm kconfig
+sudo -S pacman -Rsc --noconfirm karchive kconfig
+sudo -S pacman -Rsc --noconfirm raptor
+sudo -S pacman -Rsc --noconfirm poppler-glib
+sudo -S pacman -Rsc --noconfirm harfbuzz-icu
+sudo -S pacman -Rsc --noconfirm ark karchive plasma-desktop dolphin kate konsole attica knewstuff discover
 sudo -S cp -r /opt/claudemods-distribution-installer/pacman.conf /mnt/etc
 sudo -S pacman -Sy
 sudo -S pacman -S --noconfirm kdedevpackages

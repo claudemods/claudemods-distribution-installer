f7a9b6cf51ed72ce7ae29bea010437fe97692896
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-14 21:18:57 +0000

    more fixes

--- installer/spitfire-ckge-minimal/tweaksspitfire.sh
+++ installer/spitfire-ckge-minimal/tweaksspitfire.sh
@@ -2,7 +2,6 @@
 
 username="$1"
 
-cd /opt/claudemods-distribution-installer/spitfire-ckge-minimal && sudo -S unzip -uo SpitFireLogin.zip -d /mnt/usr/share/sddm/themes
 sudo -S chmod 4755 /usr/lib/spice-client-glib-usb-acl-helper
 echo 'blacklist ntfs3' | sudo -S tee /etc/modprobe.d/disable-ntfs3.conf >/dev/null 2>&1
 chsh -s $(which fish)

8c8c2243f4d9322d279c8ece3bea64a1582ad2e6
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-14 19:27:20 +0000

    need to test again

--- installer/spitfire-ckge-minimal/tweaksspitfire.sh
+++ installer/spitfire-ckge-minimal/tweaksspitfire.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for username in $(ls /home); do
-    target_file="/home/${username}/.local/share/user-places.xbel"
-    [ -f "$target_file" ] && sed -i "s/spitfire/${username}/g" "$target_file"
-    
-    sudo -S chmod 4755 /usr/lib/spice-client-glib-usb-acl-helper
-    echo 'blacklist ntfs3' | sudo -S tee /etc/modprobe.d/disable-ntfs3.conf >/dev/null 2>&1
-    chsh -s $(which fish)
-    sudo -S chown -R ${username}:${username} /home/${username} 
-    sudo -S chown -R ${username}:${username} /home/${username}/*
-done
+username="$1"
+
+target_file="/home/$username/.local/share/user-places.xbel"
+[ -f "$target_file" ] && sed -i "s/spitfire/$username/g" "$target_file"
+
+sudo -S chmod 4755 /usr/lib/spice-client-glib-usb-acl-helper
+echo 'blacklist ntfs3' | sudo -S tee /etc/modprobe.d/disable-ntfs3.conf >/dev/null 2>&1
+chsh -s $(which fish)
+sudo -S chown -R $username:$username /home/$username 
+sudo -S chown -R $username:$username /home/$username/*

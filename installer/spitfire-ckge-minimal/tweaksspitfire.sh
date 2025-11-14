88fd4c15c02ed2211b4758f7c1515d7555665e12
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-14 14:16:23 +0000

    should work fine

--- installer/spitfire-ckge-minimal/tweaksspitfire.sh
+++ installer/spitfire-ckge-minimal/tweaksspitfire.sh
@@ -5,5 +5,7 @@ target_file="/home/$USER/.local/share/user-places.xbel"
 
 sed -i "s/spitfire/${username}/g" "$target_file"
 
-echo 'blacklist ntfs3' | sudo tee /etc/modprobe.d/disable-ntfs3.conf >/dev/null 2>&1
-chsh -s $(which fish)
+echo 'blacklist ntfs3' | sudo -S tee /etc/modprobe.d/disable-ntfs3.conf >/dev/null 2>&1
+sudo -S chsh -s $(which fish)
+sudo -S chown -R ${username}:${username} /home/${username} 
+sudo -S chown -R ${username}:${username} /home/${username}/*

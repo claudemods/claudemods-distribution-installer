b623d2b759c02e0daca7cec77ee2a3ffe51c299a
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-16 00:32:59 +0000

    all works fine just a few packages added to kde e.g kate nano kcalc

--- installer/mainscriptdev.cpp
+++ installer/mainscriptdev.cpp
@@ -619,7 +619,7 @@ private:
 
             execute_command("cp -r /etc/resolv.conf /mnt/etc");
 
-            execute_command("pacstrap /mnt base plasma sddm dolphin konsole grub efibootmgr os-prober arch-install-scripts mkinitcpio " + selected_kernel + " linux-firmware sudo networkmanager");
+            execute_command("pacstrap /mnt base plasma sddm dolphin konsole kate nano kcalc grub efibootmgr os-prober arch-install-scripts mkinitcpio " + selected_kernel + " linux-firmware sudo networkmanager");
 
             execute_command("chroot /mnt /bin/bash -c \"systemctl enable sddm\"");
             execute_command("chroot /mnt /bin/bash -c \"systemctl enable NetworkManager\"");
